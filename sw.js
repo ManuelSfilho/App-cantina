1082dde755308816d574f97323773ed56caf4abe
ManuelSfilho <52848138+ManuelSfilho@users.noreply.github.com>
2026-06-15 05:25:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cantina-v5';
+const CACHE = 'cantina-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './produtos.xlsx', './clientes.xlsx'];
 
 self.addEventListener('install', e => {
